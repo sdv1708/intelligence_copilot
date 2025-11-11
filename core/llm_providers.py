8e286662e5a4c82c201a1e59c6ce96679d9e6a3a
Sanjay DV <55549923+sdv1708@users.noreply.github.com>
2025-11-11 11:39:01 -0500

    gemini max output set to 30000

--- core/llm_providers.py
+++ core/llm_providers.py
@@ -23,10 +23,10 @@ def get_llm_provider(provider_name: str = None):
             log_message("INFO", "Initializing Google Gemini")
             api_key = get_env("GEMINI_API_KEY")
             return ChatGoogleGenerativeAI(
-                model="gemini-2.5-flash",
+                model="	gemini-2.5-flash-lite",
                 google_api_key=api_key,
                 temperature=0.7,
-                max_output_tokens=65535
+                max_output_tokens=30000
             )
         
         elif provider_name == "openai":
